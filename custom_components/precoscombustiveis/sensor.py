a9a79fe085154d4728210ab43343e6c58193acc1
Rui Dias <ruidias@bcl.pt>
2022-06-21 00:01:46 +0100

    update indice

--- custom_components/precoscombustiveis/sensor.py
+++ custom_components/precoscombustiveis/sensor.py
@@ -106,5 +106,5 @@ class PrecosCombustiveisSensor(SensorEntity):
         if (station):
             fuel = [f for f in self._station.fuels if f["TipoCombustivel"] == self._fuelName]
             if (fuel):
-                self._state = float(fuel["Preco"].replace(" €/litro", ""))
+                self._state = float(fuel[0]["Preco"].replace(" €/litro", ""))
             
